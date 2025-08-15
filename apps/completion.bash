ae8e984db8c75703dfa579c7f1553cf4ce77a7ae
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-08-15 10:52:44 +0200

    Unset broken init completion function

--- apps/completion.bash
+++ apps/completion.bash
@@ -1,2 +1,8 @@
-# bash completion for the app named after the snap
+# Bash completion for the app named after the snap
+
+# Unset the _init_completion function from the bash-completion package to force
+# use of the basic but functional internal implementation.
+# Issue TBA
+unset -f _init_completion
+
 source <($SNAP/bin/$SNAP_NAME completion bash)
