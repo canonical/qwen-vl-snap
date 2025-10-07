dc7fa12c665bc8a2c44f5ce5b465860bea54bd75
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-10-07 17:50:29 -0300

    feat: update missing components every loop with nameref func
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- apps/server.sh
+++ apps/server.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -eu
 
+check_missing_components() {
+    local -n missing_ref=$1
+    local required_components="$2"
+
+    missing_ref=()
+    for component in $required_components; do
+        if [ ! -d "$SNAP_COMPONENTS/$component" ]; then
+            missing_ref+=("$component")
+        fi
+    done
+}
+
 engine_manifest=$(qwen-vl show-engine)
 engine="$(echo "$engine_manifest" | yq .name)"
 required_components="$(echo "$engine_manifest" | yq .components[])"
@@ -7,11 +19,7 @@ unset engine_manifest
 
 # Check for missing components
 missing_components=()
-for component in $required_components; do
-    if [ ! -d "$SNAP_COMPONENTS/$component" ]; then
-        missing_components+=("$component")
-    fi
-done
+check_missing_components missing_components "$required_components"
 
 retry_count=0
 delay=10
@@ -20,6 +28,7 @@ while [ ${#missing_components[@]} -ne 0 ] && [ "$retry_count" -lt $max_retries ]
     echo "Missing required snap components: [${missing_components[*]}], retrying in $delay seconds..."
     sleep "$delay"
     ((retry_count++))
+    check_missing_components missing_components "$required_components"
 done
 
 if [ ${#missing_components[@]} -ne 0 ]; then
