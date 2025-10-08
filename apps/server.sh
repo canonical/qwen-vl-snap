e94cdb0215b2324db8cf738f15f3bf8e4f7cd8a4
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-10-08 15:36:59 -0300

    refact: reorganize script into functions
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- apps/server.sh
+++ apps/server.sh
@@ -1,40 +1,47 @@
 #!/bin/bash -eu
 
 check_missing_components() {
-    local -n missing_ref=$1
-    local required_components="$2"
+    local -n missing=$1 # nameref for output array
+    local required="$2"
 
-    missing_ref=()
-    for component in $required_components; do
+    missing=()
+    for component in $required; do
         if [ ! -d "$SNAP_COMPONENTS/$component" ]; then
-            missing_ref+=("$component")
+            missing+=("$component")
         fi
     done
 }
 
-engine_manifest=$(qwen-vl show-engine)
-engine="$(echo "$engine_manifest" | yq .name)"
-required_components="$(echo "$engine_manifest" | yq .components[])"
-unset engine_manifest
+wait_for_components() {
+    local required_components
 
-# Check for missing components
-missing_components=()
-check_missing_components missing_components "$required_components"
+    required_components="$(qwen-vl show-engine | yq .components[])"
 
-retry_count=0
-delay=10
-max_retries=360 # 10 seconds * 360 = 1 hour
-while [ ${#missing_components[@]} -ne 0 ] && [ "$retry_count" -lt $max_retries ]; do
-    echo "Missing required snap components: [${missing_components[*]}], retrying in $delay seconds..."
-    sleep "$delay"
-    ((++retry_count))
+    missing_components=()
     check_missing_components missing_components "$required_components"
-done
 
-if [ ${#missing_components[@]} -ne 0 ]; then
-    echo "Error: timed out after $max_retries retries, missing components [${missing_components[*]}]."
-    exit 1
-fi
+    max=3600 # seconds
+    interval=10
+    elapsed=0
+    while [ ${#missing_components[@]} -ne 0 ] && [ "$elapsed" -lt $max ]; do
+        ((elapsed+=interval))
+        echo "Waiting for required snap components: [${missing_components[*]}] ($elapsed/${max}s)"
+        sleep "$interval"
 
+        check_missing_components missing_components "$required_components"
+    done
 
+    if [ ${#missing_components[@]} -ne 0 ]; then
+        echo "Error: timed out after ${elapsed}s while waiting for required components: [${missing_components[*]}]"
+        echo "Please use \"snap changes\" to monitor the progress and start the service once all components are installed."
+
+        # Stop service to avoid indefinite retries by systemd, until the next reboot
+        snapctl stop qwen-vl
+        exit 1
+    fi
+}
+
+wait_for_components
+
+engine="$(qwen-vl show-engine | yq .name)"
 exec "$SNAP/engines/$engine/server" "$@"
