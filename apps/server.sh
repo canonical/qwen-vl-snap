b4cefd85cd8337487132d476899822545b9ec34a
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-10-09 12:39:52 -0300

    Make server wait for required components (#58)
    Co-authored-by: Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>

--- apps/server.sh
+++ apps/server.sh
@@ -1,22 +1,47 @@
 #!/bin/bash -eu
 
-engine_manifest=$(qwen-vl show-engine)
-engine="$(echo "$engine_manifest" | yq .name)"
-required_components="$(echo "$engine_manifest" | yq .components[])"
-unset engine_manifest
+check_missing_components() {
+    local -n missing=$1 # nameref for output array
+    local required="$2"
 
-# Check for missing components
-missing_components=()
-for component in $required_components; do
-    if [ ! -d "$SNAP_COMPONENTS/$component" ]; then
-        missing_components+=("$component")
+    missing=()
+    for component in $required; do
+        if [ ! -d "$SNAP_COMPONENTS/$component" ]; then
+            missing+=("$component")
+        fi
+    done
+}
+
+wait_for_components() {
+    local required_components
+
+    required_components="$(qwen-vl show-engine | yq .components[])"
+
+    missing_components=()
+    check_missing_components missing_components "$required_components"
+
+    max=3600 # seconds
+    interval=10
+    elapsed=0
+    while [ ${#missing_components[@]} -ne 0 ] && [ "$elapsed" -lt $max ]; do
+        ((elapsed+=interval))
+        echo "Waiting for required snap components: [${missing_components[*]}] ($elapsed/${max}s)"
+        sleep "$interval"
+
+        check_missing_components missing_components "$required_components"
+    done
+
+    if [ ${#missing_components[@]} -ne 0 ]; then
+        echo "Error: timed out after ${elapsed}s while waiting for required components: [${missing_components[*]}]"
+        echo "Please use \"snap changes\" to monitor the progress and start the service once all components are installed."
+
+        # Stop service to avoid indefinite retries by systemd, until the next reboot
+        snapctl stop qwen-vl
+        exit 1
     fi
-done
+}
 
-if [ ${#missing_components[@]} -ne 0 ]; then
-    echo "Error: missing required snap components: [${missing_components[*]}]"
-    exit 1
-fi
+wait_for_components
 
-
+engine="$(qwen-vl show-engine | yq .name)"
 exec "$SNAP/engines/$engine/server" "$@"
