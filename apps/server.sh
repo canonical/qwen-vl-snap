41008c00f300a9595448bdf8c399e28ce6628002
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-10-13 14:15:05 -0300

    fix: reorder operations
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- apps/server.sh
+++ apps/server.sh
@@ -43,7 +43,10 @@ wait_for_components() {
 
 wait_for_components
 
+engine="$(qwen-vl show-engine | yq .name)"
+
+# Generate connection info for clients
 mkdir -p "$SNAP_DATA/share"
 qwen-vl status --format=json >"$SNAP_DATA"/share/connection.json || true
-engine="$(qwen-vl show-engine | yq .name)"
+
 exec "$SNAP/engines/$engine/server" "$@"
