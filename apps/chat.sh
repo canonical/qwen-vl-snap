ab75875f50f092edf82c23aec08be80ac9e65416
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-10-17 13:51:09 +0200

    Check status before running chat

--- apps/chat.sh
+++ apps/chat.sh
@@ -1,18 +1,23 @@
 #!/bin/bash -eu
 
-$SNAP/bin/wait-for-server.sh
+# Query status and extract the relevant info
+status_obj="$(qwen-vl status --format=json)"
 
-port="$(qwen-vl get http.port)"
-model_name="$(qwen-vl get model-name 2>/dev/null || true)" # model name isn't always set
-
-# Normally the OpenAI API is hosted under http://server:port/v1. In some cases like with OpenVINO Model Server it is under http://server:port/v3
-api_base_path="$(qwen-vl get http.base-path)"
-if [ -z "$api_base_path" ]; then
-  api_base_path="v1"
+status="$(echo "$status_obj" | jq -r '.status')"
+if [ "$status" != "online" ]; then
+  echo "Server is not online. Current status: $status"
+  exit 1
 fi
 
+openai_endpoint="$(echo "$status_obj" | jq -r '.endpoints.openai // empty')"
+if [ -z "$openai_endpoint" ]; then
+  echo "Server does not have an OpenAI endpoint."
+  exit 1
+fi
 
-export OPENAI_BASE_URL="http://localhost:$port/$api_base_path"
+model_name="$(qwen-vl get model-name 2>/dev/null || true)" # model name isn't always set
+
+export OPENAI_BASE_URL="$openai_endpoint"
 export MODEL_NAME="$model_name"
 
 $SNAP/bin/go-chat-client
