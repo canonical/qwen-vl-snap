35c711fb6676bd7fc7375a0f227ce5ffa40b1fc8
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-06-29 08:53:22 +0200

    Drop target file list for OV models

--- download-models.sh
+++ download-models.sh
@@ -36,33 +36,13 @@ hf download AmpereComputing/qwen-2.5-vl-3b-instruct-gguf \
     --local-dir components/mmproj-qwen2-5-vl-3b-instruct-aio-q8-0/
 
 # 3B OpenVINO int4 model (Intel CPU/GPU)
-OV_INT4_DIR=components/model-qwen2-5-vl-3b-instruct-ov-int4
 hf download llmware/Qwen2.5-VL-3B-Instruct-ov-int4 \
-    added_tokens.json chat_template.json config.json generation_config.json \
-    merges.txt openvino_config.json preprocessor_config.json special_tokens_map.json \
-    tokenizer.json tokenizer_config.json vocab.json \
-    openvino_detokenizer.bin openvino_detokenizer.xml \
-    openvino_language_model.bin openvino_language_model.xml \
-    openvino_text_embeddings_model.bin openvino_text_embeddings_model.xml \
-    openvino_tokenizer.bin openvino_tokenizer.xml \
-    openvino_vision_embeddings_merger_model.bin openvino_vision_embeddings_merger_model.xml \
-    openvino_vision_embeddings_model.bin openvino_vision_embeddings_model.xml \
-    --local-dir "${OV_INT4_DIR}/"
+    --local-dir components/model-qwen2-5-vl-3b-instruct-ov-int4
 # Make symlink for mediapipe graph to make it writable by OVMS when moved to a snap component
-ln -sf /tmp/graph.pbtxt "${OV_INT4_DIR}/graph.pbtxt"
+ln -sf /tmp/graph.pbtxt "components/model-qwen2-5-vl-3b-instruct-ov-int4/graph.pbtxt"
 
 # 3B OpenVINO int4 NPU model (Intel NPU)
-OV_NPU_DIR=components/model-qwen2-5-vl-3b-instruct-ov-int4-npu
 hf download llmware/Qwen2.5-VL-3B-Instruct-ov-int4-npu \
-    added_tokens.json chat_template.json config.json generation_config.json \
-    merges.txt openvino_config.json preprocessor_config.json special_tokens_map.json \
-    tokenizer.json tokenizer_config.json vocab.json \
-    openvino_detokenizer.bin openvino_detokenizer.xml \
-    openvino_language_model.bin openvino_language_model.xml \
-    openvino_text_embeddings_model.bin openvino_text_embeddings_model.xml \
-    openvino_tokenizer.bin openvino_tokenizer.xml \
-    openvino_vision_embeddings_merger_model.bin openvino_vision_embeddings_merger_model.xml \
-    openvino_vision_embeddings_model.bin openvino_vision_embeddings_model.xml \
-    --local-dir "${OV_NPU_DIR}/"
+    --local-dir components/model-qwen2-5-vl-3b-instruct-ov-int4-npu
 # Make symlink for mediapipe graph to make it writable by OVMS when moved to a snap component
-ln -sf /tmp/graph.pbtxt "${OV_NPU_DIR}/graph.pbtxt"
+ln -sf /tmp/graph.pbtxt "components/model-qwen2-5-vl-3b-instruct-ov-int4-npu/graph.pbtxt"
